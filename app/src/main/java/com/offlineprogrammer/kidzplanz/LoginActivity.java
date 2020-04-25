8db422bd112dc21d98c43919bdc8e980e3ba177a
offlineProgrammer <mmalaka@gmail.com>
2020-04-25 16:33:12 -0700

    Create a Login screen
    https://github.com/users/offlineprogrammer/projects/2#card-37010194

--- app/src/main/java/com/offlineprogrammer/kidzplanz/LoginActivity.java
+++ app/src/main/java/com/offlineprogrammer/kidzplanz/LoginActivity.java
@@ -2,15 +2,13 @@ package com.offlineprogrammer.kidzplanz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-        finish();
+        setContentView(R.layout.activity_login);
     }
 }
