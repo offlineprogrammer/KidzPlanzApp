85be9c2bfdd21cd6d8da0dc994aaf6701cd07f28
offlineProgrammer <mmalaka@gmail.com>
2020-04-28 15:20:05 -0700

    Getting DeviceToken

---
+++ app/src/main/java/com/offlineprogrammer/kidzplanz/FirebaseHelper.java
@@ -0,0 +1,76 @@
+package com.offlineprogrammer.kidzplanz;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.offlineprogrammer.kidzplanz.user.User;
+
+import java.util.ArrayList;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+
+public class FirebaseHelper {
+    private User m_User;
+    FirebaseFirestore m_db;
+    private static final String TAG = "FirebaseHelper";
+
+    public FirebaseHelper (){
+        m_db = FirebaseFirestore.getInstance();
+    }
+
+    Observable<String> getDeviceToken() {
+        return Observable.create((ObservableEmitter<String> emitter) -> {
+            FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                @Override
+                public void onSuccess(InstanceIdResult instanceIdResult) {
+                    String deviceToken = instanceIdResult.getToken();
+                    emitter.onNext(deviceToken);
+                }
+            });
+
+        });
+
+
+
+    }
+
+     Observable<User> getUserData(String deviceToken) {
+
+        return Observable.create((ObservableEmitter<User> emitter) -> {
+
+            m_db.collection("users")
+                    .whereEqualTo("deviceToken", deviceToken)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    if (document.exists()) {
+
+                                    } else {
+
+                                    }
+                                }
+
+                            } else {
+                                Log.d("Got Date", "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
+
+        });
+
+    }
+}
--- app/src/main/java/com/offlineprogrammer/kidzplanz/MainActivity.java
+++ app/src/main/java/com/offlineprogrammer/kidzplanz/MainActivity.java
@@ -3,12 +3,62 @@ package com.offlineprogrammer.kidzplanz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.offlineprogrammer.kidzplanz.user.User;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    FirebaseHelper firebaseHelper;
+    User m_User;
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseHelper = new FirebaseHelper();
+        firebaseHelper.getDeviceToken().observeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "onSubscribe");
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onNext(String deviceToken) {
+                        Log.d(TAG, "onNext: " + deviceToken);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete");
+                    }
+                });
+
+
+
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
     }
 }
