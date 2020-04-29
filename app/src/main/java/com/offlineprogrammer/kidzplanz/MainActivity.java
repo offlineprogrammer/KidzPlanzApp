d57e121141c7909f5505ddb914ed1ea3e4e19d45
offlineProgrammer <mmalaka@gmail.com>
2020-04-29 12:44:09 -0700

    Add Kid Layout

--- app/src/main/java/com/offlineprogrammer/kidzplanz/MainActivity.java
+++ app/src/main/java/com/offlineprogrammer/kidzplanz/MainActivity.java
@@ -1,13 +1,23 @@
 package com.offlineprogrammer.kidzplanz;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 import com.offlineprogrammer.kidzplanz.kid.Kid;
 import com.offlineprogrammer.kidzplanz.kid.KidAdapter;
 import com.offlineprogrammer.kidzplanz.kid.KidGridItemDecoration;
@@ -15,6 +25,9 @@ import com.offlineprogrammer.kidzplanz.kid.OnKidListener;
 import com.offlineprogrammer.kidzplanz.user.User;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
@@ -42,6 +55,89 @@ public class MainActivity extends AppCompatActivity implements OnKidListener {
         getDeviceToken();
 
 
+    }
+
+    private void configActionButton() {
+        FloatingActionButton fab = findViewById(R.id.fab_add_kid);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddKidDialog(MainActivity.this);
+            }
+        });
+    }
+
+    private int pickMonster(){
+        final TypedArray imgs;
+        imgs = getResources().obtainTypedArray(R.array.kidzMonsters);
+        final Random rand = new Random();
+        final int rndInt = rand.nextInt(imgs.length());
+        return imgs.getResourceId(rndInt, 0);
+    }
+
+    private void showAddKidDialog(Context c) {
+
+        final AlertDialog builder = new AlertDialog.Builder(c).create();
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_dialog_add_kid, null);
+        final TextInputLayout kidNameText = dialogView.findViewById(R.id.kidname_text_input);
+        kidNameText.requestFocus();
+        Button okBtn= dialogView.findViewById(R.id.kidname_save_button);
+        Button cancelBtn = dialogView.findViewById(R.id.kidname_cancel_button);
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String kidName = String.valueOf(kidNameText.getEditText().getText());
+                if (!isKidNameValid(kidName)) {
+                    kidNameText.setError(getString(R.string.kid_error_name));
+                } else {
+                    kidNameText.setError(null);
+                    Date currentTime = Calendar.getInstance().getTime();
+                    int monsterImage = pickMonster();
+                    String monsterImageResourceName = getResources().getResourceEntryName(monsterImage);
+
+                    Kid newKid = new Kid(kidName,
+                            monsterImage,
+                            monsterImageResourceName,
+                            currentTime);
+                    setupProgressBar();
+                   // newKid = saveKid(newKid);
+                    Log.i(TAG, "onClick UserFireStore : " + newKid.getUserFirestoreId());
+                    Log.i(TAG, "onClick KidFireStore : " + newKid.getFirestoreId());
+                    mAdapter.add(newKid, 0);
+                    recyclerView.scrollToPosition(0);
+                  //  mFirebaseAnalytics.logEvent("kid_created", null);
+                    builder.dismiss();
+                }
+
+
+            }
+        });
+
+        kidNameText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                String kidName = String.valueOf(kidNameText.getEditText().getText());
+                if (isKidNameValid(kidName)) {
+                    kidNameText.setError(null); //Clear the error
+                }
+                return false;
+            }
+        });
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                builder.dismiss();
+            }
+        });
+        builder.setView(dialogView);
+        builder.show();
+        builder.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+    }
+
+    private void setupProgressBar() {
+    }
+
+    private boolean isKidNameValid(String kidName) {
+        return kidName != null && kidName.length() >= 2;
     }
 
     private void setupRecyclerView() {
@@ -104,8 +200,12 @@ public class MainActivity extends AppCompatActivity implements OnKidListener {
                         m_User = user;
                         if (m_User.getFirebaseId()==null) {
                             saveUser();
-                        } else {}
-                        getKidzData(m_User.getFirebaseId());
+                        } else {
+                            getKidzData(m_User.getFirebaseId());
+                        }
+
+                        configActionButton();
+
 
                     }
 
