8b6fdeee48e9d780f0bdae1fa0c7b028636209f4
offlineProgrammer <mmalaka@gmail.com>
2020-04-30 12:04:14 -0700

    Kid layout

--- app/src/main/java/com/offlineprogrammer/kidzplanz/KidActivity.java
+++ app/src/main/java/com/offlineprogrammer/kidzplanz/KidActivity.java
@@ -3,12 +3,35 @@ package com.offlineprogrammer.kidzplanz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.offlineprogrammer.kidzplanz.kid.Kid;
 
 public class KidActivity extends AppCompatActivity {
+
+    private static final String TAG = "KidActivity";
+    ImageView kidImageView;
+    TextView kidNameTextView;
+    Kid selectedKid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kid);
+
+        kidImageView = findViewById(R.id.kidMonsterImage);
+        kidNameTextView = findViewById(R.id.kidnameTextView);
+
+        if (getIntent().hasExtra("selected_kid")) {
+            // setupProgressBar();
+            Bundle data = getIntent().getExtras();
+            selectedKid = data.getParcelable("selected_kid");
+            kidImageView.setImageResource(selectedKid.getMonsterImage());
+            kidNameTextView.setText(selectedKid.getKidName());
+
+        }
+
     }
 }
